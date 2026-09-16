72b883d12dac5e3fe2287b381435d2e2c73f8609
haegh <haegh@haegh.com>
2026-09-16 21:51:03 +0200

    trying fix screens 3

--- boards/shields/sharp_mip/widgets/util.c
+++ boards/shields/sharp_mip/widgets/util.c
@@ -18,10 +18,10 @@ void rotate_canvas(lv_obj_t *canvas) {
     const uint32_t stride = lv_draw_buf_width_to_stride(CANVAS_SIZE, CANVAS_COLOR_FORMAT);
 #ifdef CONFIG_SHARP_MIP_ROTATE_180
     lv_draw_sw_rotate(buf_copy, buf, CANVAS_SIZE, CANVAS_SIZE, stride, stride,
-                      LV_DISPLAY_ROTATION_270, CANVAS_COLOR_FORMAT);
+                      LV_DISPLAY_ROTATION_90, CANVAS_COLOR_FORMAT);
 #else
     lv_draw_sw_rotate(buf_copy, buf, CANVAS_SIZE, CANVAS_SIZE, stride, stride,
-                      LV_DISPLAY_ROTATION_90, CANVAS_COLOR_FORMAT);
+                      LV_DISPLAY_ROTATION_270, CANVAS_COLOR_FORMAT);
 #endif
 }
 
